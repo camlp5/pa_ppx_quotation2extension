16fa42366a1ce03319314683a1a3d0cd06b4c6dc
Chet Murthy <chetsky@gmail.com>
2023-03-13 14:22:45 -0700

    deal with location
    that is, properly handle <:expr:<...>>
    
    via [%expr.loc {| ... |}]

--- pa_quotation2extension/pa_ppx_quotation2extension.ml
+++ pa_quotation2extension/pa_ppx_quotation2extension.ml
@@ -1,4 +1,4 @@
-(**pp -syntax camlp5o -package camlp5.extfun,camlp5.parser_quotations *)
+(**pp -syntax camlp5o -package camlp5.extfun,camlp5.parser_quotations,pa_ppx_regexp *)
 (* camlp5o *)
 (* pa_string.ml,v *)
 (* Copyright (c) INRIA 2007-2017 *)
@@ -13,21 +13,41 @@ let has_quotation ename =
       _ -> true
     | exception Not_found -> false
 
+let handle_expr_quotation loc ename payload =
+  let (ename,located) =
+    match [%match {|^(.*)\.loc$|} / strings !1] ename with
+      None -> (ename, false)
+    | Some ename -> (ename, true) in
+  if has_quotation ename then
+    let ename = ename^(if located then "@" else ":") in
+    Some(Pcaml.handle_expr_quotation loc (ename, payload))
+  else
+    None
+
+let handle_patt_quotation loc ename payload =
+  let (ename,located) =
+    match [%match {|^(.*)\.loc$|} / strings !1] ename with
+      None -> (ename, false)
+    | Some ename -> (ename, true) in
+  if has_quotation ename then
+    let ename = ename^(if located then "@" else ":") in
+    Some(Pcaml.handle_patt_quotation loc (ename, payload))
+  else
+    None
+
 let rewrite_expr_extension arg = function
   <:expr:< [%$attrid:(_,ename)$ $str:payload$ ] >> as z ->
-   if has_quotation ename then
-     Pcaml.handle_expr_quotation loc (ename, payload)
-   else
-     z
+   (match handle_expr_quotation loc ename payload with
+      Some e -> e
+    | None -> z)
 | e -> Fmt.(raise_failwithf (MLast.loc_of_expr e) "pa_ppx_quotation2extension: payload of a [%%<extension> ...] expr-extension must be a single string: %a"
             Pp_MLast.pp_expr e)
 
 let rewrite_patt_extension arg = function
   <:patt:< [%$attrid:(_,ename)$ $str:payload$ ] >> as z ->
-   if has_quotation ename then
-     Pcaml.handle_patt_quotation loc (ename, payload)
-   else
-     z
+   (match handle_patt_quotation loc ename payload with
+      Some e -> e
+    | None -> z)
 | e -> Fmt.(raise_failwithf (MLast.loc_of_patt e) "pa_ppx_quotation2extension: payload of a [%%<extension> ...] patt-extension must be a single string: %a"
             Pp_MLast.pp_patt e)
 
--- tests/pa_ppx_quotation2extension_test.ml
+++ tests/pa_ppx_quotation2extension_test.ml
@@ -8,7 +8,12 @@ let loc = Ploc.dummy
 let test_simple ctxt =
   ()
   ; assert_equal ~cmp:Reloc.eq_expr <:expr< 1 >> <:expr< 1 >>
+
   ; assert_equal ~cmp:Reloc.eq_expr <:expr< 1 >> [%expr {| 1 |}]
+  ; assert_equal () (match [%expr {| 1 |}] with [%expr {| 1 |}] -> ())
+
+  ; assert_equal () (match <:expr< 1 >> with <:expr:< 1 >> -> ignore(loc) ; ())
+  ; assert_equal () (match [%expr.loc {| 1 |}] with [%expr.loc {| 1 |}] -> ignore(loc) ; ())
 
 let suite = "Test pa_ppx_quotation2extension" >::: [
       "simple"   >:: test_simple
